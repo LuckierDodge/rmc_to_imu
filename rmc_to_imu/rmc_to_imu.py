d49e63a6f972676ead2810d20514eda7af8bfb29
Ryan D. Lewis <rdlrobot@umich.edu>
2023-04-04 13:10:21 -0400

    Support message repeating

--- rmc_to_imu/rmc_to_imu.py
+++ rmc_to_imu/rmc_to_imu.py
@@ -5,36 +5,40 @@ from nmea_msgs.msg import Sentence
 from geometry_msgs.msg import Quaternion
 from tf_transformations import quaternion_from_euler
 
+
 class Rmc_To_Imu(Node):
     def __init__(self):
         super().__init__("rmc_to_imu")
 
         self.imu_publisher_ = self.create_publisher(Imu, "heading", 10)
-        self.nmea_subscriber_ = self.create_subscription(Sentence, "nmea", self.nmea_callback, 10)
+        self.nmea_subscriber_ = self.create_subscription(
+            Sentence, "nmea", self.nmea_callback, 10
+        )
+        self.timer = self.create_timer(1, self.timer_callback)
+        self.last_msg = None
 
     def nmea_callback(self, msg):
         sentence = msg.sentence
         if "RMC" in sentence:
-            split = sentence.split(',')
+            split = sentence.split(",")
             cog = split[8]
             # self.get_logger().info(f"{split}")
-            if cog != '':
+            if cog != "":
+                self.last_msg = msg
                 q = quaternion_from_euler(
                     0,
                     0,
-                    -1 * float(cog) * 3.1415926535 / 180 + (3.1415926535 / 2) # Convert to ENU
+                    -1 * float(cog) * 3.1415926535 / 180
+                    + (3.1415926535 / 2),  # Convert to ENU
                 )
-                self.imu_publisher_.publish(
-                    Imu(
-                        orientation = Quaternion(
-                            x = q[0],
-                            y = q[1],
-                            z = q[2],
-                            w = q[3]
-                        )
-                    )
-                )
+                msg = Imu(orientation=Quaternion(x=q[0], y=q[1], z=q[2], w=q[3]))
+                msg.header.frame_id = "gps"
+                self.imu_publisher_.publish(msg)
                 # self.get_logger().info(f"{cog}, {float(cog) * 3.1415926535 / 180}, ({q[2]}, {q[3]})")
+
+    def timer_callback(self):
+        if self.last_msg is not None:
+            self.nmea_callback(self.last_msg)
 
 def main(args=None):
     rclpy.init(args=args)
@@ -49,5 +53,5 @@ def main(args=None):
     rclpy.shutdown()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- setup.py
+++ setup.py
@@ -1,26 +1,23 @@
 from setuptools import setup
 
-package_name = 'rmc_to_imu'
+package_name = "rmc_to_imu"
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version="0.0.0",
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='mcity',
-    maintainer_email='rdlrobot@umich.edu',
-    description='Takes in NMEA sentences, parses RMC messages from them, and converts it to an absolute position IMU.',
-    license='MIT License',
-    tests_require=['pytest'],
+    maintainer="mcity",
+    maintainer_email="rdlrobot@umich.edu",
+    description="Takes in NMEA sentences, parses RMC messages from them, and converts it to an absolute position IMU.",
+    license="MIT License",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-            'rmc_to_imu = rmc_to_imu.rmc_to_imu:main'
-        ],
+        "console_scripts": ["rmc_to_imu = rmc_to_imu.rmc_to_imu:main"],
     },
 )
