3e109f9904d803b3d4d535cac040eab400fa071b
Ryan D. Lewis <rdlrobot@umich.edu>
2023-04-11 18:52:14 -0400

    Publish both Heading and IMU

--- rmc_to_imu/rmc_to_imu.py
+++ rmc_to_imu/rmc_to_imu.py
@@ -1,16 +1,23 @@
 import rclpy
 from rclpy.node import Node
+
 from sensor_msgs.msg import Imu
 from nmea_msgs.msg import Sentence
+
 from geometry_msgs.msg import Quaternion
 from tf_transformations import quaternion_from_euler
+from mcity_proxy_msgs.msg import Heading
+
+import math
+import numpy as np
 
 
 class Rmc_To_Imu(Node):
     def __init__(self):
         super().__init__("rmc_to_imu")
 
-        self.imu_publisher_ = self.create_publisher(Imu, "heading", 10)
+        self.heading_publisher_ = self.create_publisher(Heading, "/heading", 10)
+        self.imu_publisher_ = self.create_publisher(Imu, "/imu/heading", 10)
         self.nmea_subscriber_ = self.create_subscription(
             Sentence, "nmea", self.nmea_callback, 10
         )
@@ -25,11 +32,18 @@ class Rmc_To_Imu(Node):
             # self.get_logger().info(f"{split}")
             if cog != "":
                 self.last_msg = msg
+                heading = -1.0 * math.radians(float(cog)) + (np.pi / 2.0) # * Convert to Radians ENU
+                while heading > 2.0 * np.pi:
+                    heading -= 2.0 * np.pi
+                while heading < 0.0:
+                    heading += 2.0 * np.pi
+
+                msg = Heading(heading=heading)
+                self.heading_publisher_.publish(msg)
                 q = quaternion_from_euler(
                     0,
                     0,
-                    -1 * float(cog) * 3.1415926535 / 180
-                    + (3.1415926535 / 2),  # Convert to ENU
+                    heading
                 )
                 msg = Imu(orientation=Quaternion(x=q[0], y=q[1], z=q[2], w=q[3]))
                 msg.header.frame_id = "gps"
@@ -39,6 +53,7 @@ class Rmc_To_Imu(Node):
     def timer_callback(self):
         if self.last_msg is not None:
             self.nmea_callback(self.last_msg)
+
 
 def main(args=None):
     rclpy.init(args=args)
